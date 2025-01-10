ebed84db9888ff6bc1d5b12b59fd042050b4bf26
vsualzm <ims.ilham@gmail.com>
2025-01-10 16:15:18 +0700

    update go routine

---
+++ go-concurrency/main.go
@@ -0,0 +1,23 @@
+package main
+
+//  go concurrency & parrarel programming berbeda
+
+// parrarel programming
+// process vs thread
+// parrarel (secara bersamaan ) vs concurrency (menjalankan beberapa pekerjaan secara bergantian)
+
+//  a --> []
+//  b --> []
+
+// cpu bound (Machine learning)
+// parrarel bound
+// io bound
+
+// go routine -> thread ringan di kelola go runtime
+// ukurangnya sangat kecil di bandungkan thread
+//  2kb vs 1mb
+//
+
+func main() {
+
+}
